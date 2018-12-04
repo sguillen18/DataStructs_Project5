4470c098aa1f2d06be66996a1d0bb41abfb5953a
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-12-04 14:00:40 -0500

    Add files via upload

--- KDTree.java
+++ KDTree.java
@@ -1,4 +1,19 @@
 
 public class KDTree {
 
+	private Coordinates root;
+	
+	public KDTree() {
+		root = null;
+	}
+	
+	public KDTree(int x, int y) {
+		root.setXCoordinate(x);
+		root.setYCoordinate(y);
+	}
+	
+	public boolean isEmpty() {
+		return root == null;
+	}
+	
 }
